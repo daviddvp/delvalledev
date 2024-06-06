18b90476266615ae09b479d564d3b072384fe5a7
daviddvp <delvallepuadodavid@gmail.com>
2024-06-06 17:52:06 +0200

    Nueva version minimalista

--- src/utils/AppConfig.ts
+++
@@ -1,9 +0,0 @@
-export const AppConfig = {
-  site_name: 'DelValleDev',
-  title: 'DelValleDev',
-  description: 'DelValleDev built with Astro using React and Tailwind CSS',
-  // description: 'DelValleDev'
-  author: 'David Del Valle',
-  locale_region: 'es-ES',
-  locale: 'es-ES',
-};
